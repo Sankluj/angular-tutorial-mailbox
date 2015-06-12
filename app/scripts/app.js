cd2ca054e087e79212063a1681a031c8d44ef50b
Matthieu Kluj <matthieu.kluj@sogeti.lu>
2015-06-12 15:25:31 +0200

    App Scaffolding, Module and Data Binding Examples

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularTutorialMailboxApp
+ * @description
+ * # angularTutorialMailboxApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularTutorialMailboxApp', ['ui.bootstrap']);
