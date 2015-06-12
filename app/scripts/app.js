f45c4dea7cb421f305ad656545b8ddbd75fdaafb
Matthieu Kluj <matthieu.kluj@sogeti.lu>
2015-06-12 16:29:27 +0200

    Add MailboxCtrl and retrieve and display  a list of messages from a json file
    Demonstrate Controllers, Dependency Injections, $http and $log service, and some angular in built directives

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,31 @@
  * Main module of the application.
  */
 angular
-  .module('angularTutorialMailboxApp', ['ui.bootstrap']);
+  .module('angularTutorialMailboxApp', [
+    'ui.bootstrap'
+  ])
+  .controller('MailboxCtrl', function ($scope, $http, $log) {
+    // List Of Messages
+    $scope.messages = [];
+
+    // Retrieve all the messages from messages.json
+    $scope.getMessages = function() {
+      // Use the angular $http service to get the messages
+      $http.get('assets/messages.json')
+      .success(function(messagesData) {
+        $log.info(messagesData.length + ' Messages Retrieved')
+        $scope.messages = messagesData;
+      })
+      .error(function(errorData) {
+        $log.error('Get Messages Error.')
+      });
+    };
+
+    $scope.markMessageAsRead = function(message) {
+      message.markedAsRead = true;
+    };
+
+    // Call the Get messages function on controller initialisation
+    $scope.getMessages();
+
+  });
